636c53a6b98f76adb3468ae8b5a7275730a9f683
SvetlanaZhurba <43250637+SvetlanaZhurba@users.noreply.github.com>
2019-05-01 13:34:04 +0300

    Create queue.h

---
+++ queue.h
@@ -0,0 +1,18 @@
+#include "node.h"
+
+class queue
+{
+private:
+  node *end;
+  int cnt;
+public:
+  queue();
+  ~queue();
+  void show();
+  node* operator = ( queue &q );
+  queue& operator + ( queue &q );
+  queue& operator * ( queue &q );
+  queue& operator / ( int n );
+  friend queue& operator - ( queue &q1, queue &q2 );
+  friend queue& operator += ( queue &q, int n );
+};
