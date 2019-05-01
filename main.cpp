7ce2f5e1d19b654a546c15f0b9328c3aae12f129
SvetlanaZhurba <43250637+SvetlanaZhurba@users.noreply.github.com>
2019-05-01 13:29:52 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,115 @@
+#include <iostream>
+#include "q_time.h"
+#include "queue.h"
+
+using namespace std;
+
+int main()
+{
+  int c;
+  setlocale( LC_ALL, "Rus" );
+
+  system( "cls" );
+  cout << "Выберите задание: ";
+  cin >> c;
+  cin.ignore( 1, '\n' );
+
+  if( c == 1 )
+  {
+    time t;
+    int choice;
+  
+    do
+    {
+      system( "cls" );
+      cout << "Настоящее время: " << t.gethour() << ":" << t.getmin() << ":" << t.getsec() << endl << endl;
+      cout << "Выберите действие:\n1. Увеличить выбранный показатель на единицу.\n2. Уменьшить выбранный показатель на единицу."
+      << "\n3. Увеличить выбранный показатель на ваше значение.\n4. Уменьшить выбранный показатель на ваше значение.\nВаш выбор: ";
+      cin >> choice;
+      cin.ignore( 1, '\n' );
+
+      switch( choice )
+      {
+      case 1:
+        ++t;
+        break;
+
+      case 2:
+        --t;
+        break;
+
+      case 3:
+        t++;
+        break;
+
+      case 4:
+        t--;
+        break;
+      }
+    } while( choice != 0 );
+  }
+  else
+  {
+    queue q1, q2;
+    int choice, flag;
+    int elem;
+    node* p;
+
+    do
+    {
+      system( "cls" );
+      cout << "Выберите действие:\n1. Добавить элемент в очередь.\n2. Извлечь элемент из очереди.\n3. Сложение двух очередей.\n";
+      cout << "4. Вычитание очередей.\n5. Перемножение очередей.\n6. Деление очереди на число.\n0. Выход." << endl;
+      cin >> choice;
+      cin.ignore( 1, '\n' );
+      cout << "Выберите очередь для работы: ";
+      cin >> flag;
+      cin.ignore( 1, '\n' );
+
+      switch( choice )
+      {
+      case 1:
+        cout << "Введите значение: ";
+        cin >> elem;
+        cin.ignore( 1,'\n' );
+
+        if( flag == 1 )
+          q1 += elem;
+        else
+          q2 += elem;
+        break;
+
+      case 2:
+        //p = q1;
+        break;
+
+      case 3:
+        q1 + q2;
+        break;
+
+      case 4:
+        q1 - q2;
+        break;
+
+      case 5:
+        q1 * q2;
+        break;
+
+      case 6:
+        cout << "Введите значение: ";
+        cin >> elem;
+        cin.ignore( 1,'\n' );
+
+        if( flag == 1 )
+          q1 / elem;
+        else
+          q2 / elem;
+        break;
+      }
+      
+      getchar();
+    } while( choice != 0 );
+  }
+  
+  return 0;
+}
